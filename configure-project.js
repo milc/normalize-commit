0c06c22128c886cf6ec986bfb0ff47e669e992b4
Frédérique Milc <frederique@milc.fr>
2019-05-06 21:38:38 +0200

    Update postinstall
    Initialize commitizen to use cz-conventional-changelog.
    Edit the package to add the husky config.
    Display help text if no .gitignore file is found

--- configure-project.js
+++ configure-project.js
@@ -13,7 +13,7 @@ commitlint.examples.js
 
 if(!process.cwd().endsWith("node_modules/normalize-commit")) {
   console.log('> Script is not running from a node_modules folder');
-  console.log('>> ✔ Config files copy is disabled');
+  console.log('>> ✔ Stop without modification');
   console.log('--------------------------------------------------');
   return;
 } 
@@ -28,8 +28,9 @@ fs.copySync(path.resolve(__dirname,'./commitlint.examples.js'), '../../commitlin
 console.log('--------------------------------------------------');
 
 fs.readFile(GITIGNORE, function (err, data) {
-  if (err) throw err;
-  if(data.includes('## ------ normalize-commit ------ ##')){
+  if (err) {
+   console.log('>> No .gitignore found. You may want to exclude those files: .huskyrc.js, commitlint.config.js, commitlint.examples.js');
+  } else if(data.includes('## ------ normalize-commit ------ ##')){
    console.log('>> ✔ You already have config files in your .gitignore. You may want to exclude those files: .huskyrc.js, commitlint.config.js, commitlint.examples.js');
   } else {
     console.log('>> ✔ Add config files in your .gitignore ');
@@ -37,6 +38,29 @@ fs.readFile(GITIGNORE, function (err, data) {
   }
 });
 
-exec('commitizen init cz-conventional-changelog --save-dev --save-exact').on('exit', (e) => {
-  console.log('>> ✔ Init conventional-changelog, your package.json may have been modified');
+// Modify package.json with a config entry (needed)
+
+// This command edit the package to add the husky config
+exec(`node ../../node_modules/json/lib/json.js -I -f ../../package.json -e 'this.config={"husky": {"path": ".huskyrc.js"}}'`, (error, stderr, stdout) => {
+  if(error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log('>> Add Husky config, your package.json may have been modified');
+  // if(stdout) { console.log(`stdout: ${stdout}`); }
+  // if(stderr) { console.log(`stderr: ${stderr}`); }
 });
+
+// This command do all config steps, including update the package.json
+exec('node_modules/.bin/commitizen init cz-conventional-changelog --save --save-exact --force', 
+  {cwd: '../../'},
+  (error, stderr, stdout) => {
+    if(error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log('>> Add conventional-changelog, your package.json may have been modified');
+    // if(stdout) { console.log(`stdout: ${stdout}`); }
+    // if(stderr) { console.log(`stderr: ${stderr}`); }
+  }
+);
