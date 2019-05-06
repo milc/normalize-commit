d519a633ba85b21c8aa21e71734dd2ce5a9021e0
Frédérique Milc <frederique@milc.fr>
2019-05-06 20:04:45 +0200

    Update postinstall : add config files to .gitignore

--- configure-project.js
+++ configure-project.js
@@ -2,6 +2,14 @@
 const fs = require('fs-extra');
 const path = require('path');
 const exec = require('child_process').exec;
+const GITIGNORE = '../../.gitignore';
+const ignoreFiles = `
+## ------ normalize-commit ------ ##
+.huskyrc.js
+commitlint.config.js
+commitlint.examples.js
+## ------ /normalize-commit ------ ##
+`;
 
 if(!process.cwd().endsWith("node_modules/normalize-commit")) {
   console.log('> Script is not running from a node_modules folder');
@@ -18,3 +26,13 @@ fs.copySync(path.resolve(__dirname,'./commitlint.config.js'), '../../commitlint.
 fs.copySync(path.resolve(__dirname,'./commitlint.examples.js'), '../../commitlint.examples.js');
 
 console.log('--------------------------------------------------');
+
+fs.readFile(GITIGNORE, function (err, data) {
+  if (err) throw err;
+  if(data.includes('## ------ normalize-commit ------ ##')){
+   console.log('>> ✔ You already have config files in your .gitignore. You may want to exclude those files: .huskyrc.js, commitlint.config.js, commitlint.examples.js');
+  } else {
+    console.log('>> ✔ Add config files in your .gitignore ');
+    fs.appendFile(GITIGNORE, ignoreFiles, 'utf8');
+  }
+});
